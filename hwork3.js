b5c6ddfd926442526473c622b0ce96b0f4f69fcd
OGlushak <126804299+OGlushak@users.noreply.github.com>
2023-04-02 17:39:55 -0400

    hwork3 final

--- hwork3.js
+++ hwork3.js
@@ -32,3 +32,59 @@ function randArray(k) {
 randArray(5);
 
 //Task4
+function raiseToDegree(a, b) {
+    let result = a ** b;
+    console.log(result);
+}
+raiseToDegree(3, 4);
+
+//Task5
+function findMin() {
+    console.log(arguments);
+    let number = Math.min(12, 14, 4, -4, 0.2);
+    console.log(number);
+}
+findMin()
+
+Task6
+
+function findUnic(arr) {
+    for (let a = 0; a < arr.length; a++) {
+        for (let b = 0; b < arr.length; b++) {
+            if (arr[a] === arr[b]) {
+                return false;
+            } if (arr[a] !== arr[b]) {
+                return true;
+            }
+        }
+    }
+}
+console.log(findUnic([1, 2, 3, 5, 11]));
+
+Task7
+function lastElement(arr, count) {
+    if (count === null) {
+        console.log(arr.pop());
+    }
+    else {
+        console.log(arr.slice(-count));
+    }
+}
+console.log(lastElement([3, 4, 10, -5]));
+console.log(lastElement([3, 4, 10, -5], 2));
+console.log(lastElement([3, 4, 10, -5], 8));
+
+Task8
+function upperCase(sentence) {
+    const words = sentence.split(" ")
+
+    const newArray = []
+    for (const word of words) {
+        let firstLetter = word.substring(0, 1) // getting first letter of each word
+        firstLetter = firstLetter.toUpperCase()
+        let restLetters = word.substring(1)
+        newArray.push(firstLetter + restLetters)
+    }
+    return newArray.join(" ")
+}
+console.log(upperCase(`i love java script`))
